d15a7aeb3efadb32f4901622721f6bf5db8b1d88
Katya Samsonova <2715309@gmail.com>
2022-09-08 14:57:10 +0300

    Task g solution

---
+++ task_g/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+long double calc(int n, int m) {
+
+    std::vector<std::vector<long double> > dp(n + 1, std::vector<long double> (m + 1, 0.0));
+
+    dp[0][0] = 1.0;
+
+    for(int i = 0; i < n; ++i) {
+
+        for(int j = 0; j < m; ++j) {
+
+            for(int k = 1; k <= 6; ++k) {
+
+                if(j + k <= m) {
+                    dp[i + 1][j + k] += dp[i][j] / 6;
+                }
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+int main()
+{
+    int n, m;
+
+    std::cin >> n >> m;
+
+    double ans = calc(n, m);
+
+    std::cout << std::fixed << std::setprecision(9) << ans << std::endl;
+    return 0;
+}
