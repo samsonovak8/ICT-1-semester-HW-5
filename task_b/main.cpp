1e41730fe769839bb28be447b26322db55e7361f
*•.¸♡ Katya  ✿.｡.:* ☆:** <89536093+samsonovak8@users.noreply.github.com>
2022-10-02 18:55:09 +0300

    Task b solution

--- task_b/main.cpp
+++ task_b/main.cpp
@@ -18,17 +18,17 @@ void calc(size_t n, size_t s, std::vector<int> a, std::vector<int> &dp, std::vec
 }
 
 int main() {
-    size_t n;
-    std::cin >> n;
-    std::vector<int> a(n);
-    for (size_t i = 0; i < n; ++i) {
+    int *n = new int;
+    std::cin >> *n;
+    std::vector<int> a(*n);
+    for (size_t i = 0; i < *n; ++i) {
         std::cin >> a[i];
     }
 
     size_t s, inf = 1e9+7;
     std::cin >> s;
     std::vector<int> dp(s + 1, inf), pred(s + 1);
-    calc(n, s, a, dp, pred);
+    calc(*n, s, a, dp, pred);
     if (dp[s] == inf) {
         std::cout << -1;
     }
@@ -41,5 +41,7 @@ int main() {
                 std::cout << " ";
         }
     }
+    delete n;
+    n = nullptr;
     return 0;
 }
