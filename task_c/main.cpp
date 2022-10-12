86ffdbd19b6f67a00b29b3ce4282045461a97c73
*•.¸♡ Katya  ✿.｡.:* ☆:** <89536093+samsonovak8@users.noreply.github.com>
2022-10-12 11:05:54 +0300

    remove *n + fine array names

--- task_c/main.cpp
+++ task_c/main.cpp
@@ -1,40 +1,37 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-int calc(size_t n, std::vector<int> a) {
+int calc_min_ropes_length(size_t n, std::vector<int> a) {
 
-    std::vector<int> dp(n);
-    dp[0] = 1e9+7;
-    dp[1] = a[1] - a[0];
+    std::vector<int> min_length(n);
+    min_length[0] = 1e9+7;
+    min_length[1] = a[1] - a[0];
 
     for (size_t i = 2; i < n; ++i) {
 
-        dp[i] = std::min(dp[i - 2], dp[i - 1]) + a[i] - a[i - 1];
+        min_length[i] = std::min(min_length[i - 2], min_length[i - 1]) + a[i] - a[i - 1];
     }
 
-    return dp[n - 1];
+    return min_length[n - 1];
 }
 
 int main() {
 
-    int *n = new int;
+    int n;
 
-    std::cin >> *n;
+    std::cin >> n;
 
-    std::vector<int> a(*n);
+    std::vector<int> a(n);
 
-    for (size_t i = 0; i < *n; ++i) {
+    for (size_t i = 0; i < n; ++i) {
 
         std::cin >> a[i];
     }
 
     std::sort(a.begin(), a.end());
 
-    std::cout << calc(*n, a);
-
-    delete n;
-    n = nullptr;
+    std::cout << calc_min_ropes_length(n, a);
 
     return 0;
 }
